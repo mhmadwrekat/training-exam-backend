79a18391349ad2305db92671c7b13127acf51ea2
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-10-02 16:43:12 +0300

    finish

--- controllers/digimon.controller.js
+++ controllers/digimon.controller.js
@@ -1,7 +1,7 @@
 'use strict' ;
 const Axios = require('axios') ;
-const DigimonModel = require('../models/digimon.models') ;
-
+const { DigModel , DigimonModel } = require('../models/digimon.models');
+////////////////////// API
 const getData = async (req, res) => {
     let Url = 'https://digimon-api.vercel.app/api/digimon';
     let axiosRes = await Axios.get(Url);
@@ -11,5 +11,47 @@ const getData = async (req, res) => {
     })
     res.status(200).json( cleanData );
 }
-
-module.exports = getData ;
+////////////////////// Retern All Data From DB
+let bookController = (req, res) => {
+    DigModel.find().then(data => {
+        res.status(200).json(data);
+    })
+}
+//////////////////////////////// Post New  
+const createBookController = async (req, res) => {
+    let bookData = req.body;
+    let newBook = DigModel(bookData);
+    newBook.save();
+    let data = await DigModel.find({});
+    res.status(201).json(data);
+}
+//////////////////////////////// UPDATE
+const updateBookController = async (req, res) => {
+    let bookId = req.params.id;
+    let update = req.body;
+    DigModel.findOne({ _id: bookId }).then(item => {
+        item.name = update.name,
+            item.img = update.img,
+            item.level = update.level
+        item.save();
+    });
+    let up = await DigModel.find({});
+    res.status(200).send(up);
+}
+////////////////////////////// DELETE
+const deleteBookController = (req, res) => {
+    let id = req.params.id;
+    DigModel.findByIdAndDelete(id, (error, data) => {
+        if (error) {
+            res.status(500).send("ERROR ❌");
+        } else {
+            DigModel.find({}).then(books => {
+                res.json(books);
+            })}})}
+module.exports = {
+    getData,
+    updateBookController,
+    deleteBookController,
+    createBookController,
+    bookController
+} ;
--- models/digimon.models.js
+++ models/digimon.models.js
@@ -1,7 +1,29 @@
 'use strict' ;
-class DigimonModel {constructor(name, img, level) {
+const mongoose = require('mongoose');
+class DigimonModel {
+    constructor(name, img, level) {
     this.name = name;
     this.img = img;
     this.level = level;
 }}
-module.exports = DigimonModel;
+/////////// DB ////////////
+const SCM = new mongoose.Schema({
+    name: String,
+    img: String,
+    level: String
+});
+let seed = () => {
+    let newDig = new DigModel({
+        name: "name test",
+        img: "img test",
+        level: 'level test'
+    });
+    newDig.save();
+}
+const DigModel = mongoose.model('test', SCM);
+
+module.exports = {
+    DigimonModel, 
+    DigModel,
+    seed
+};
