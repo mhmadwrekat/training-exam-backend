69807944afef2919205418de9a5adb87eb615af8
mhmad wrekat <mhmmadwreekkat@gmail.com>
2021-10-02 23:00:58 +0300

    deploy

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ app.use(cors());
 app.use(express.json());
 const mongoose = require('mongoose');
 const PORT = process.env.PORT ;
-mongoose.connect('mongodb://localhost:27017/test1', { useNewUrlParser: true, useUnifiedTopology: true });
-app.listen(PORT, () => {console.log(`✔️ You In Port : ${PORT}✔️!`);})
+const MONGO_SERVER = process.env.MONGO_SERVER ;
+mongoose.connect(`mongodb+srv://${MONGO_SERVER}?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
 const { getData,
     bookController,
     createBookController,
@@ -22,3 +22,4 @@ app.post('/create', createBookController);
 app.put('/update/:id', updateBookController)
 app.delete('/delete/:id', deleteBookController);
 /////////////////////////////////////////////////////
+app.listen(PORT, () => {console.log(`✔️ You In Port : ${PORT}✔️!`);})
